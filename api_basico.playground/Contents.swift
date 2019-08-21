ef70e32ae50da069fc6e0f3722273c36d2f1b53d
Wolfgang Walder <wolfwalder@gmail.com>
2019-08-21 15:25:45 -0300

    Minhas modificações 1

--- api_basico.playground/Contents.swift
+++ api_basico.playground/Contents.swift
@@ -1,9 +1,16 @@
 import Foundation
 
+struct Place: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var completed: Bool
+}
+
 
 //Não esqueca de importar o Foundation
 func getRequest(url: String,
-                completion: @escaping ([String: Any]?, Error?) -> Void){
+                completion: @escaping (Place?, Error?) -> Void){
     //URL válida
     guard let URL = URL(string: url) else {
         completion(nil, nil)
@@ -22,17 +29,17 @@ func getRequest(url: String,
     //Cria a tarefa de requisição
     let task = URLSession.shared.dataTask(with: request as URLRequest) {
         (data, response, error) in
+        
+        guard let data = data, error == nil else {
+            completion(nil,nil)
+            return
+        }
+        
         do {
             
-            if let data = data {
-                //A resposta chegou
-                let response = try JSONSerialization.jsonObject(with: data, options: [])
-                completion(response as? [String : Any], nil)
-            }
-            else {
-                //Não houve resposta
-                completion(nil, nil)
-            }
+            let response = try JSONDecoder().decode(Place.self, from: data)
+            completion(response, nil)
+            
         } catch let error as NSError {
             // Houve um erro na comunicao com o servidor
             completion(nil, error)
@@ -48,16 +55,15 @@ func getRequest(url: String,
 
 //Coloque a URL da sua API aqui
 let url = "https://jsonplaceholder.typicode.com/todos/1"
+//let url = "https://uselessfacts.jsph.pl/random.json?language=en"
 
 
 //Chamando a funcão GET
 getRequest(url: url){
     (resultado, erro)  in
-    if(resultado != nil) {
-        //O resultado aqui vem como Opcional
-        print("Sua requisicao foi realizada com sucesso: \n \(String(describing: resultado))")
-    } else {
-        print("A requisicao nao funcionou")
+    guard let resultado = resultado else {
+        print("Arequisição não funcionou")
+        return
     }
-    
+    print(resultado.userId)
 }
